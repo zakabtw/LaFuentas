c68a2b4f362562376fd3be7c523924337964ffe3
zakabtw <zakabtw@outlook.com>
2026-05-04 21:17:25 +0200

    Update leaderboard.js

--- src/commands/Leveling/leaderboard.js
+++ src/commands/Leveling/leaderboard.js
@@ -32,7 +32,7 @@ export default {
         await InteractionHelper.safeEditReply(interaction, {
           embeds: [
             new EmbedBuilder()
-              .setColor('#f1c40f')
+              .setColor('#8B0000')
               .setDescription('The leveling system is currently disabled on this server.')
           ],
           flags: MessageFlags.Ephemeral
